2d6c6f25ab0f3246c2a40149a994e50da918ca5a
Sandeep Kadyan <sandeep.kadyan@gmail.com>
2017-09-20 18:46:32 +0100

    added vert.x rx dependency

--- resiliency-core/src/test/java/com/skadyan/resilience/SimpleExampleTest.java
+++ resiliency-core/src/test/java/com/skadyan/resilience/SimpleExampleTest.java
@@ -1,13 +1,24 @@
 package com.skadyan.resilience;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+@Tag("slow")
 public class SimpleExampleTest {
 
     @Test
     void myFirstTest() {
         assertEquals(2, 1 + 1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Hello", "World" })
+    void testWithStringParameter(String argument) {
+        assertNotNull(argument);
     }
 }
