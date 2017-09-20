30ba42eeb4211af20e910dffe731555c9c04d32a
Sandeep Kadyan <sandeep.kadyan@gmail.com>
2017-09-20 16:16:49 +0100

    added example of setting up junit5 in maven pom.xml and running in eclipse oxygen

---
+++ resiliency-core/src/test/java/com/skadyan/resilience/SimpleExampleTest.java
@@ -0,0 +1,13 @@
+package com.skadyan.resilience;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleExampleTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
